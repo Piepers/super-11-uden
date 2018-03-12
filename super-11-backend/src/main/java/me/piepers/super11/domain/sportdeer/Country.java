3d9eafe83339c7d2a457a2a65cfb841469fbcae5
Bas Piepers <bas@piepers.me>
2018-03-12 20:05:54 +0100

    More domain object updates.

--- super-11-backend/src/main/java/me/piepers/super11/domain/sportdeer/Country.java
+++ super-11-backend/src/main/java/me/piepers/super11/domain/sportdeer/Country.java
@@ -5,20 +5,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.core.json.JsonObject;
-import me.piepers.super11.domain.JsonDomainObject;
 
 @DataObject
 public class Country extends SportdeerDomainObject {
-	
+
 	@JsonProperty("name")
-	private String name;
+	private final String name;
 	@JsonProperty("square_flag")
-	private String sFlagLink;
+	private final String sFlagLink;
 	@JsonProperty("rectangle_flag")
-	private String rFlagLink;
-	
+	private final String rFlagLink;
+
 	@JsonCreator
-	public Country(@JsonProperty("_id") Long id, @JsonProperty("name") String name, @JsonProperty("square_flag") String sFlagLink, @JsonProperty("rectangle_flag") String rFlagLink) {
+	public Country(@JsonProperty("_id") Long id, @JsonProperty("name") String name,
+			@JsonProperty("square_flag") String sFlagLink, @JsonProperty("rectangle_flag") String rFlagLink) {
 		super(id);
 		this.name = name;
 		this.sFlagLink = sFlagLink;
@@ -27,51 +27,60 @@ public class Country extends SportdeerDomainObject {
 
 	public Country(JsonObject jsonObject) {
 		super(jsonObject);
-		this.name = docs.getString("name");
-		this.sFlagLink = docs.getString("square_flag");
-		this.rFlagLink = docs.getString("rectangle_flag");		
+		this.name = this.docs.getString("name");
+		this.sFlagLink = this.docs.getString("square_flag");
+		this.rFlagLink = this.docs.getString("rectangle_flag");
 	}
 
-	
 	@Override
 	public String toString() {
-		return "Country [id=" + getId() + ", name=" + name + ", sFlagLink=" + sFlagLink + ", rFlagLink=" + rFlagLink + "]";
+		return "Country [id=" + this.getId() + ", name=" + this.name + ", sFlagLink=" + this.sFlagLink + ", rFlagLink="
+				+ this.rFlagLink + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((rFlagLink == null) ? 0 : rFlagLink.hashCode());
-		result = prime * result + ((sFlagLink == null) ? 0 : sFlagLink.hashCode());
+		result = prime * result + (this.name == null ? 0 : this.name.hashCode());
+		result = prime * result + (this.rFlagLink == null ? 0 : this.rFlagLink.hashCode());
+		result = prime * result + (this.sFlagLink == null ? 0 : this.sFlagLink.hashCode());
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (!super.equals(obj))
+		}
+		if (!super.equals(obj)) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (this.getClass() != obj.getClass()) {
 			return false;
+		}
 		Country other = (Country) obj;
-		if (name == null) {
-			if (other.name != null)
+		if (this.name == null) {
+			if (other.name != null) {
 				return false;
-		} else if (!name.equals(other.name))
+			}
+		} else if (!this.name.equals(other.name)) {
 			return false;
-		if (rFlagLink == null) {
-			if (other.rFlagLink != null)
+		}
+		if (this.rFlagLink == null) {
+			if (other.rFlagLink != null) {
 				return false;
-		} else if (!rFlagLink.equals(other.rFlagLink))
+			}
+		} else if (!this.rFlagLink.equals(other.rFlagLink)) {
 			return false;
-		if (sFlagLink == null) {
-			if (other.sFlagLink != null)
+		}
+		if (this.sFlagLink == null) {
+			if (other.sFlagLink != null) {
 				return false;
-		} else if (!sFlagLink.equals(other.sFlagLink))
+			}
+		} else if (!this.sFlagLink.equals(other.sFlagLink)) {
 			return false;
+		}
 		return true;
-	}	
+	}
 }
--- super-11-backend/src/main/java/me/piepers/super11/domain/sportdeer/SportdeerDomainObject.java
+++ super-11-backend/src/main/java/me/piepers/super11/domain/sportdeer/SportdeerDomainObject.java
@@ -13,26 +13,56 @@ public abstract class SportdeerDomainObject implements JsonDomainObject {
 
 	@JsonProperty("_id")
 	private Long id;
-	
+
 	@JsonIgnore
 	protected JsonObject docs;
-	
+
 	@JsonCreator
 	public SportdeerDomainObject(@JsonProperty("_id") Long id) {
 		this.id = id;
 	}
-	
+
 	public SportdeerDomainObject(JsonObject jsonObject) {
 		this.docs = jsonObject.getJsonObject("docs");
-		this.id = docs.getLong("_id");
+		this.id = this.docs.getLong("_id");
 	}
 
 	public Long getId() {
-		return id;
+		return this.id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (this.id == null ? 0 : this.id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		SportdeerDomainObject other = (SportdeerDomainObject) obj;
+		if (this.id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!this.id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
 }
