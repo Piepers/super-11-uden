3d78ecd5cbf2b1fd0eb49c282fc379a1c6b67b67
Bas Piepers <bas@piepers.me>
2018-03-24 21:37:15 +0100

    Removed irrelevant test, minor additions.

--- super-11-backend/src/main/java/me/piepers/super11/Super11Application.java
+++ super-11-backend/src/main/java/me/piepers/super11/Super11Application.java
@@ -1,25 +1,24 @@
 package me.piepers.super11;
 
-import io.vertx.reactivex.core.AbstractVerticle;
-import me.piepers.super11.application.FeedVerticle;
-import me.piepers.super11.application.HttpServerVerticle;
-import me.piepers.super11.application.Super11CommandHandlerVerticle;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
-import io.vertx.reactivex.config.ConfigRetriever;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.reactivex.Completable;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.reactivex.core.AbstractVerticle;
+import me.piepers.super11.application.FeedVerticle;
+import me.piepers.super11.application.HttpServerVerticle;
+import me.piepers.super11.application.Super11CommandHandlerVerticle;
 
 /**
  * The main entry point for the application. Used as the configuration class of
  * the application. See pom.xml file.
- * 
+ *
  * @author Bas Piepers (bas@piepers.me)
  *
  */
@@ -36,26 +35,26 @@ public class Super11Application extends AbstractVerticle {
 				.setConfig(new JsonObject().put("path", "super-11-conf.json"));
 		final ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(store);
 
-		final ConfigRetriever configRetriever = ConfigRetriever.create(vertx, options);
+		final ConfigRetriever configRetriever = ConfigRetriever.create(this.vertx, options);
 
 		// Deploy verticles
 		configRetriever.rxGetConfig().flatMapCompletable(configuration ->
 
 		Completable.fromAction(() -> LOGGER.info("Deploying super 11 backend"))
-				.andThen(vertx.rxDeployVerticle(Super11CommandHandlerVerticle.class.getName(),
+				.andThen(this.vertx.rxDeployVerticle(Super11CommandHandlerVerticle.class.getName(),
 						new DeploymentOptions().setConfig(configuration)))
 				.toCompletable()
-				.andThen(vertx.rxDeployVerticle(HttpServerVerticle.class.getName(),
+				.andThen(this.vertx.rxDeployVerticle(HttpServerVerticle.class.getName(),
 						new DeploymentOptions().setConfig(configuration)))
-				.toCompletable().andThen(vertx.rxDeployVerticle(FeedVerticle.class.getName(),
+				.toCompletable().andThen(this.vertx.rxDeployVerticle(FeedVerticle.class.getName(),
 						new DeploymentOptions().setConfig(configuration)))
 				.toCompletable()
 
 		).subscribe(() -> {
-			LOGGER.info("Machine mock has been deployed successfully.");
+			LOGGER.info("Super 11 application has been deployed successfully.");
 			startFuture.complete();
 		}, throwable -> {
-			LOGGER.error("Machine mock has not been deployed due to: ", throwable);
+			LOGGER.error("Super 11 has not been deployed due to: ", throwable);
 			startFuture.fail(throwable);
 		});
 	}
